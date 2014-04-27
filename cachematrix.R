910566264b3f4bfa78b5a0d701ebca62156671af
Steve Jobs <elkk@lboro.ac.uk>
2014-04-27 21:58:41 +0100

    Fix typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
-## A couple of first class functions (similar to the concept of object in Perl/Python
+## A couple of first class functions (similar to the concept of object in Perl/Python)
 ## are built to allow for a "special" type of matrix object. What is special about
-## this object is that by using makeCacheMatrix, it can store, apart from the matrix,
-## the inverse matrix, and the functions to read and write the inverse. The inverse
-## can be calculated using the functon cacheSolve
+## this object is that by using makeCacheMatrix, it can store, apart from the matrix 
+## itself, the inverse matrix, and the functions to read and write the inverse. 
+## The inverse can be calculated using the functon cacheSolve
 
 ## This function simply stores a matrix as a "special matrix" object. 
 ## The  "special matrix" object, apart from holding the matrix, also holds
-## it's inverse (initially NULL), and can also read and write the matrix itself
+## it's inverse (initially set to NULL), and can also read and write the matrix itself
 ## and its inverse using the get, set, set_inverse and get_inverse functions
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -27,6 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## a "special matrix" and if the inverse has not been calculated before, it goes
 ## to calculate it and stores the inverse in the "special matrix". Otherwise, it 
 ## returns the already calculated inverse of the "special matrix". 
+
 cacheSolve <- function(x, ...) {
     inverse <- x$get_inverse()
     if (!is.null(inverse)) {
